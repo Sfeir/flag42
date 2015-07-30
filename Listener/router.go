55ace3f743f49f258109df20686d0233cca64252
Dorian Couly <doriancouly@MacBook-Air-de-Dorian.local>
2015-07-30 14:16:55 +0200

    Correction de bug : pas de doublons dans datastore

--- Listener/router.go
+++ Listener/router.go
@@ -7,15 +7,16 @@ import (
 	"github.com/carbocation/go-instagram/instagram"
 	"github.com/gorilla/mux"
 	"io"
+	"appengine/datastore"
+	"appengine/memcache"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"util"
 
 	"database/controllers"
+	"database/models"
 )
-
-var titime int64
 
 func NewPic(w http.ResponseWriter, req *http.Request) {
 	m, _ := url.ParseQuery(req.URL.RawQuery)
@@ -37,26 +38,39 @@ func NewPic(w http.ResponseWriter, req *http.Request) {
 	client.ClientSecret = "fb8a88e4743144b39b011a402a4a66c2"
 	res, _, err := client.Tags.RecentMedia(resp[0].ObjectID, nil)
 	if err == nil {
-		var tempTime int64
-		for _, v := range res {
-			if v.CreatedTime > titime {
-				if tempTime == 0 || tempTime < v.CreatedTime {
-					tempTime = v.CreatedTime
-				}
-				url := v.Images.StandardResolution.URL
-				rgb, err := util.PixColor(url, c)
-				if err != nil {
-					json.NewEncoder(w).Encode(err.Error())
-				}
-				err = controllers.AddImage(url, rgb, c)
-				if err != nil {
-					json.NewEncoder(w).Encode(err.Error())
-				}
+		i := 0
+		for true {
+			v := res[i]
+			i++
+			item, err := memcache.Get(c, "last")
+			url := v.Images.StandardResolution.URL
+			if err == nil && string(item.Value) == url {
+				break
+			}
+			q := datastore.NewQuery("image").Filter("Link =", url)
+			var link []models.Image
+			q.GetAll(c, &link)
+			if len(link) != 0 {
+				break
+			}
+			rgb, err := util.PixColor(url, c)
+			if err != nil {
+				json.NewEncoder(w).Encode(err.Error())
+			}
+			err = controllers.AddImage(url, rgb, c)
+			if err != nil {
+				json.NewEncoder(w).Encode(err.Error())
 			}
 		}
-		titime = tempTime
 	} else {
 		json.NewEncoder(w).Encode(err.Error())
+	}
+	cache := &memcache.Item{
+		Key:   "last",
+		Value: []byte(res[0].Images.StandardResolution.URL),
+	}
+	if err := memcache.Add(c, cache); err != nil{
+		memcache.Set(c, cache)
 	}
 }
 
