ad528d6c97931eefce320fff5b09b0f69efa0131
Elizaveta <lizvlasova@gmail.com>
2015-07-30 15:06:45 +0200

    affichage dales

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,18 @@
 var init = function() {
-    //creation de la structure
 
+};
+
+var init_structure = function() {
+    //creation de la structure
+    for (var line = 0; line < 10; line++) {
+        var section_name = 'section_'+line;
+        $("#main").append("<div id=\'"+section_name+"\' class='section group'>");
+        for (var colomn = 0; colomn < 10; colomn++) {
+            var span_name = 'span_'+colomn;
+            $("#"+section_name).append("<div id=\'"+section_name+'_'+span_name+"\' class='col span_1_of_10'>");
+        }
+        $("#main").append( "</div>" );
+    }
 };
 
 var zoom_plus = function() {
