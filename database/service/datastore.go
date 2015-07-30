c5ce9d370c75985a7cbf326aa88685d0bc4116c2
Romain Abidonn <romain.abidonn@gmail.com>
2015-07-30 11:55:16 +0200

    resize + changing palette to 256 colors + comments

--- database/service/datastore.go
+++ database/service/datastore.go
@@ -8,7 +8,9 @@ import (
 )
 
 func AddData(img models.Image, c appengine.Context) (string, error) {
+	//create a new key for our data
 	key := datastore.NewIncompleteKey(c, "image", nil)
+	//store in the datastore
 	a, err := datastore.Put(c, key, &img)
 	return a.String(), err
 }
@@ -16,12 +18,17 @@ func AddData(img models.Image, c appengine.Context) (string, error) {
 func GetData(c appengine.Context, id int, count int) ([]models.Image, error) {
 	var listImg []models.Image
 
+	//create a query for the datastore
 	q := datastore.NewQuery("image")
+	//filter by choosen color
 	q = q.Filter("Color =", id)
+	//order by most recently added image
 	q = q.Order("-Date")
+	//limit to number of needed images (if -1 get all images)
 	if count != -1 {
 		q = q.Limit(count)
 	}
+	//get data from the datastore
 	if _, err := q.GetAll(c, &listImg); err != nil {
 		return nil, err
 	} else {
