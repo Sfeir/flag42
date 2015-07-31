fc8fcdd09202f38f8a1f3258763bfb6db2a12b2a
Romain Abidonn <romain.abidonn@gmail.com>
2015-07-31 11:02:23 +0200

    algo de creation d'image + router compute

---
+++ compute/Listener/router.go
@@ -0,0 +1,26 @@
+package Listener
+
+import (
+	//"appengine"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/url"
+
+	"storage"
+)
+
+func storeImages(w http.ResponseWriter, req *http.Request) {
+	m, _ := url.ParseQuery(req.URL.RawQuery)
+	dir := m["dir"][0]
+	store := m["storage"][0]
+	ComputeToStorage(dir, store)
+	// c := appengine.NewContext(req)
+	// c.Infof("%d", len(str))
+	// io.WriteString(w, string(json))
+}
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/store", storeImages)
+	http.Handle("/", r)
+}
