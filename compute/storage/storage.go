ac17defe29befa7f184279fc8fd4492f66c3fb28
Romain Abidonn <romain.abidonn@gmail.com>
2015-07-31 17:03:14 +0200

    ok

--- compute/storage/storage.go
+++ compute/storage/storage.go
@@ -4,14 +4,19 @@ import (
 	"os/exec"
 )
 
-func ComputeToStorage(dir, store string){
+func ComputeToStorage(dir, store string) {
+	print("dir :")
+	print(dir)
+	print("\nstore :")
+	print(store)
 	cmd := exec.Command("gsutil", "cp", "-R", dir, store)
+	print("\n\nCaca")
 	cmd.Run()
-	cmd = exec.Command("gsutil","-m","acl","set","-R","-a","public-read", store)
+	cmd = exec.Command("gsutil", "-m", "acl", "set", "-R", "-a", "public-read", store)
 	cmd.Run()
 }
 
-func StorageToCompute(dir, store string){
+func StorageToCompute(dir, store string) {
 	cmd := exec.Command("gsutil", "cp", "-R", store, dir)
 	cmd.Run()
 }
